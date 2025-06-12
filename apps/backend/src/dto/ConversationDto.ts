0193e4b7b63beab968079b84dd94dd159e3dcd48
ize2589 <ize2589@gmail.com>
2025-06-12 10:25:38 +0800

    Revert "feat:完成conversations"
    This reverts commit 065eabaac739b4fbd5edc63d423f2922799c8488.

--- apps/backend/src/dto/ConversationDto.ts
+++
@@ -1,6 +0,0 @@
-export interface ConversationDto {
-  id: string;
-  title: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
--- apps/backend/src/services/ConversationService.ts
+++
@@ -1,8 +0,0 @@
-import { ConversationRepository } from '../repository/ConversationRepository'
-import { Conversation } from '../db/entity/ConversationEntity'
-
-const conversationRepository = new ConversationRepository()
-
-export const createConversation = async (title: string): Promise<Conversation> => {
-  return await conversationRepository.createConversation(title)
-}
